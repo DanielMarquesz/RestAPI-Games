67de54cf9d8b5f2daed0b6a03980b6bcaaec8d4c
Daniel Marques <daniel.marques142@gmail.com>
2021-01-19 00:57:46 -0300

    fix: fixing db connection syntax code

--- APIGames/database/connection.js
+++ APIGames/database/connection.js
@@ -1,17 +1,3 @@
-/*
-
-const Sequellize = require('sequelize');
-
-const configDatabase = require('../config/db.config');
-
-
-const connection = new Sequellize(configDatabase);
-const Games = require("../models/Games");
-
-Games.init(connection);
-
-module.exports = connection;
-*/
 
 const configDatabase = require('../config/db.config')
 const Sequelize = require('sequelize')
